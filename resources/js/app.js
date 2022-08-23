d8ae0070564a4192e8ec753439564c7f7977b79e
Rakibul Hasan Rabbi <rakibul.cse.47@gmail.com>
2022-08-23 12:09:50 +0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import "./bootstrap";
+
+import { createApp } from "vue";
+import App from "./layouts/app.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
---
+++ resources/js/layouts/app.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+This is a Laravel 9 and Vue js 3 projects.
+</div>
+</template>
